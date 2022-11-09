136a4c8ee253c919b095350a4cf9d3dbe82a20c2
Priti Kumari <75195316+priti27599@users.noreply.github.com>
2022-11-09 19:20:03 +0530

    commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function onClickMenu() {
+    document.getElementById("nav").classList.toggle("change");
+    //document.getElementById("menu").src = "./assets/images/icon-menu-close.svg";
+    
+    
+}
+
+
